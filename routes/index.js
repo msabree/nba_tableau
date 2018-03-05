64a0a4858a5ce63ca16dca48503f77aa455cffc9
Makeen Sabree <makeen.sabree@gmail.com>
2018-03-05 18:49:55 -0500

    Pull down box score for games airing on current date.

--- routes/index.js
+++ routes/index.js
@@ -5,33 +5,88 @@ var request = require('request');
 var btoa = require('btoa');
 var fs = require('fs');
 
-// helper functions
+const NBA_SEASON = 'current'; // Change here for other options (e.g. 'current', 'latest', 'upcoming', 2016-playoff', '2016-2017-regular')
+const AUTH = btoa('fake:fake'); // don't put stuff like this in github!
+const FORMAT = 'json';
+
+// get the games going on today (be careful with server date and games happening late.. server date may roll over to next day)
 const getDailyGameSchedule = function() {
 
   var options = {
-    url: `https://api.mysportsfeeds.com/v1.2/pull/nba/current/daily_game_schedule.json?fordate=${moment().format('YYYYMMDD')}`,
+    url: `https://api.mysportsfeeds.com/v1.2/pull/nba/${NBA_SEASON}/daily_game_schedule.${FORMAT}?fordate=${moment().format('YYYYMMDD')}`,
     headers: {
-      Authorization: `Basic ${btoa('FAKE:FAKE')}`, // don't put stuff like this in github!
+      Authorization: `Basic ${AUTH}`, 
     }
   };
 
   return new Promise((resolve, reject) => {
     request(options, (error, response, body) => {
       if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        fs.writeFile('game_schedule.json', body, (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!');
-        });
+        resolve(JSON.parse(body));
+      }
+      else{
+        reject({error, code: response.statusCode});
       }
     });
   });
 }
 
+// Fetch on an n-second interval
+const startFetcher = function(arrGameIds) {
+  console.log('new pull initiated');
+  console.log(arrGameIds);
+  const boxScorePromises = [];
+  for(let i = 0; i < arrGameIds.length; i++){
+    // Generate a dynamic url
+    var options = {
+      url: `https://api.mysportsfeeds.com/v1.2/pull/nba/${NBA_SEASON}/game_boxscore.${FORMAT}?gameid=${arrGameIds[i]}`,
+      headers: {
+        Authorization: `Basic ${AUTH}`, // don't put stuff like this in github!
+      }
+    };
+    console.log(options.url);
+    boxScorePromises.push(new Promise((resolve, reject) => {
+      request(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          resolve(JSON.parse(body));
+        }
+        else if(response.statusCode === 404){
+          resolve({custom_message: 'Game may not have started yet.'})
+        }
+        else{
+          reject({error, code: response.statusCode});
+        }
+      });
+    }));
+  }
+
+  Promise.all(boxScorePromises).then((arrJsonResponses) => {
+    console.log(arrJsonResponses.length)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  getDailyGameSchedule();
+  getDailyGameSchedule()
+  .then((dailyGameJSON) => {
+    const games = dailyGameJSON.dailygameschedule.gameentry;
+    const ids = [];
+    for(let i = 0; i < games.length; i++){
+      const gameId = `${moment().format('YYYYMMDD')}-${games[i].awayTeam.Abbreviation}-${games[i].homeTeam.Abbreviation}`;
+      ids.push(gameId);
+    }
+
+    // CREATE REFRESHER HERE
+    const refreshEverySecs = 5;
+    setInterval(startFetcher.bind(null, ids), refreshEverySecs * 1000);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 
   res.render('index', { title: 'Express' });
 });
