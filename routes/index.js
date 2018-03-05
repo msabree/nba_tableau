82234089eb233779df5d11fba7eb429d93de9e8e
Makeen Sabree <makeen.sabree@gmail.com>
2018-03-05 17:10:19 -0500

    Pulling daily game schedule.

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+var request = require('request');
+var btoa = require('btoa');
+var fs = require('fs');
+
+// helper functions
+const getDailyGameSchedule = function() {
+
+  var options = {
+    url: `https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/daily_game_schedule.json?fordate=${moment().format('YYYYMMDD')}`,
+    headers: {
+      Authorization: `Basic ${btoa('fake:fake')}`, // don't put stuff like this in github!
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        fs.writeFile('game_schedule.json', body, (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+      }
+    });
+  });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  getDailyGameSchedule();
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
